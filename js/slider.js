03f0df0b3b3aa165bd84dff07a180f9b5bdda97a
Leonel <leonelperez.b23@gmail.com>
2022-05-31 20:41:26 -0300

    arreglar version responsive

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,56 @@
-$(document).ready(function(){
-    //seleccionando elementos en variables//
-   var slider = $('#slider');
-    var btnSiguiente = $('#btnSiguiente');
-    var btnAtras = $('#btnAtras');
+$(document).ready(function () {
 
-    //trasladando la ultima imagen antes de la primera//
+  // Seleccionando elementos en variables
+  var slider = $('#slider');
+  var btnAnterior = $('#btnAtras');
+  var btnSiguiente = $('#btnSiguiente');
 
-    $('#slider .slide:last').insertBefore('#slider .slide:first');
+  // Se pasa el último slide al primer lugar
+  $('#slider .slide:last').insertBefore('#slider .slide:first');
 
-    //margen negativo y mostrar el primer slide//
-    slider.css('margin-left','-28%');
+  // Con margen negativo se vuelve a mostrar el primer slide
+  slider.css('margin-left', '-46.5%');
 
-
-    //function siguiente///
-    function moverDerecha(){
-        if(!slider.is(':animated')){
-            slider.animate({marginLeft:'-80.3%'},700,function(){
-                $('#slider .slide:first').insertAfter('#slider .slide:last');
-                slider.css('margin-left','-28%');
-                resetInterval();
-            });
-        }
-        
+  // Funcion para que el slide se mueva a la derecha
+  function moverDerecha() {
+    if (!slider.is(':animated')) {
+      slider.animate({
+        marginLeft: '-111.3%'
+      }, 700, function () {
+        $('#slider .slide:first').insertAfter('#slider .slide:last');
+        slider.css('margin-left', '-46.5%');
+        resetInterval();
+      });
     }
-    btnSiguiente.on('click',moverDerecha);
 
-    //function atras//
-    function moverIzquierda(){
-       if(!slider.is(':animated')){
-           //trasladando la ultima imagen antes de la primera//
-        $('#slider .slide:last').insertAfter('#slider .slide:first');
-        slider.css('margin-left','-80.3%');
+  }
 
-        slider.animate({marginLeft:'-28%'},700, function(){
-            resetInterval();
-        });
-       }
+  btnSiguiente.on('click', moverDerecha);
 
+  // Funcion para que el slide se mueva a la izquierda
+  function moverIzquierda() {
+    if (!slider.is(':animated')) {
+      $('#slider .slide:last').insertBefore('#slider .slide:first');
+      slider.css('margin-left', '-111.3%');
+      slider.animate({
+        marginLeft: '-46.5%'
+      }, 700, function () {
+        resetInterval()
+      });
     }
-    btnAtras.on('click',moverIzquierda)
 
-//intervalo para slide automatico//
+  }
 
-var intervalo = setInterval(moverDerecha,5000);
-function resetInterval(){
+  btnAnterior.on('click', moverIzquierda);
+
+
+
+  // Intervalo para que el slide se mueva automáticamente
+  var intervalo = setInterval(moverDerecha, 5000);
+
+  function resetInterval(){
     clearInterval(intervalo);
-    intervalo = setInterval(moverDerecha,5000);
- }
+    intervalo = setInterval(moverDerecha, 5000);
+  }
 
 });
-
--- js/slider2.js
+++ js/slider2.js
@@ -1,51 +1,56 @@
-$(document).ready(function(){
-    //seleccionando elementos en variables//
-   var slider = $('#slider2');
-    var btnSiguiente = $('#btnSiguiente2');
-    var btnAtras = $('#btnAtras2');
+$(document).ready(function () {
 
-    //trasladando la ultima imagen antes de la primera//
+  // Seleccionando elementos en variables
+  var slider = $('#slider2');
+  var btnAnterior = $('#btnAtras2');
+  var btnSiguiente = $('#btnSiguiente2');
 
-    $('#slider2 .slide:last').insertBefore('#slider2 .slide:first');
+  // Se pasa el último slide al primer lugar
+  $('#slider2 .slide:last').insertBefore('#slider2 .slide:first');
 
-    //margen negativo y mostrar el primer slide//
-    slider.css('margin-left','-28%');
+  // Con margen negativo se vuelve a mostrar el primer slide
+  slider.css('margin-left', '-46.5%');
 
-
-    //function siguiente///
-    function moverDerecha(){
-        if(!slider.is(':animated')){
-            slider.animate({marginLeft:'-80.3%'},700,function(){
-                $('#slider2 .slide:first').insertAfter('#slider2 .slide:last');
-                slider.css('margin-left','-28%');
-                resetInterval();
-            });
-        }
-        
+  // Funcion para que el slide se mueva a la derecha
+  function moverDerecha() {
+    if (!slider.is(':animated')) {
+      slider.animate({
+        marginLeft: '-111.3%'
+      }, 700, function () {
+        $('#slider2 .slide:first').insertAfter('#slider2 .slide:last');
+        slider.css('margin-left', '-46.5%');
+        resetInterval();
+      });
     }
-    btnSiguiente.on('click',moverDerecha);
 
-    //function atras//
-    function moverIzquierda(){
-       if(!slider.is(':animated')){
-           //trasladando la ultima imagen antes de la primera//
-        $('#slider2 .slide:last').insertAfter('#slider2 .slide:first');
-        slider.css('margin-left','-80.3%');
+  }
 
-        slider.animate({marginLeft:'-28%'},700, function(){
-            resetInterval();
-        });
-       }
+  btnSiguiente.on('click', moverDerecha);
 
+  // Funcion para que el slide se mueva a la izquierda
+  function moverIzquierda() {
+    if (!slider.is(':animated')) {
+      $('#slider2 .slide:last').insertBefore('#slider2 .slide:first');
+      slider.css('margin-left', '-111.3%');
+      slider.animate({
+        marginLeft: '-46.5%'
+      }, 700, function () {
+        resetInterval()
+      });
     }
-    btnAtras.on('click',moverIzquierda)
 
-//intervalo para slide automatico//
+  }
 
-var intervalo = setInterval(moverDerecha,5000);
-function resetInterval(){
+  btnAnterior.on('click', moverIzquierda);
+
+
+
+  // Intervalo para que el slide se mueva automáticamente
+  var intervalo = setInterval(moverDerecha, 5000);
+
+  function resetInterval(){
     clearInterval(intervalo);
-    intervalo = setInterval(moverDerecha,5000);
- }
+    intervalo = setInterval(moverDerecha, 5000);
+  }
 
 });
